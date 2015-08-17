f7ceff2996b671db826e39d59cf095ee4aa96b69
Andrew Thomas <andrewt1993@yahoo.com>
2015-08-16 20:51:10 -0500

    Formatting changes

--- QKSMS/src/main/java/com/moez/QKSMS/ui/conversationlist/ConversationListFragment.java
+++ QKSMS/src/main/java/com/moez/QKSMS/ui/conversationlist/ConversationListFragment.java
@@ -196,8 +196,9 @@ public class ConversationListFragment extends QKFragment implements LoaderManage
 
         dialog.addMenuItem(R.string.menu_delete_conversation, MENU_DELETE_CONVERSATION);
 
-        if(conversation.hasError())
+        if (conversation.hasError()) {
             dialog.addMenuItem(R.string.delete_failed, MENU_DELETE_FAILED);
+        }
 
         dialog.buildMenu(new AdapterView.OnItemClickListener() {
             @Override
