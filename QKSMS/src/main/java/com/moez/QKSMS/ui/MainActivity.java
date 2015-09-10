06e3037f32c75e7546b674b21da23d65105ec4f6
l3d00m <thomasvanhalen01@gmail.com>
2015-09-10 17:57:34 +0200

    add a non intrusive rate snackbar

--- QKSMS/src/main/java/com/moez/QKSMS/ui/MainActivity.java
+++ QKSMS/src/main/java/com/moez/QKSMS/ui/MainActivity.java
@@ -26,10 +26,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+
 import com.google.android.mms.pdu_alt.PduHeaders;
 import com.moez.QKSMS.R;
 import com.moez.QKSMS.common.ConversationPrefsHelper;
 import com.moez.QKSMS.common.DonationManager;
+import com.moez.QKSMS.common.QksmsRateSnack;
 import com.moez.QKSMS.common.google.DraftCache;
 import com.moez.QKSMS.common.utils.KeyboardUtils;
 import com.moez.QKSMS.common.utils.MessageUtils;
@@ -55,6 +57,9 @@ import com.moez.QKSMS.ui.view.slidingmenu.SlidingMenu;
 import com.moez.QKSMS.ui.welcome.WelcomeActivity;
 import com.nispok.snackbar.Snackbar;
 import com.nispok.snackbar.listeners.ActionClickListener;
+
+import org.ligi.snackengage.SnackEngage;
+import org.ligi.snackengage.snacks.BaseSnack;
 
 import java.net.URLDecoder;
 import java.util.Collection;
@@ -121,6 +126,10 @@ public class MainActivity extends QKActivity implements SlidingMenu.OnOpenListen
         onNewIntent(getIntent());
 
         showDialogIfNeeded(savedInstanceState);
+
+        //Adds a small/non intrusive snackbar that asks the user to rate the app
+        SnackEngage.from(MainActivity.this).withSnack(
+                new QksmsRateSnack().withDuration(BaseSnack.DURATION_LONG)).build().engageWhenAppropriate();
     }
 
     /**
