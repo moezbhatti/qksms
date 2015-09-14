0d9fad29466eaf35573fca3ecb18def57beeb5b6
Moez Bhatti <moez.bhatti@gmail.com>
2015-09-13 21:05:47 -0400

    Fix test case

--- QKSMS/src/androidTest/java/com/moez/QKSMS/common/utils/DateFormatterTest.java
+++ QKSMS/src/androidTest/java/com/moez/QKSMS/common/utils/DateFormatterTest.java
@@ -1,14 +1,11 @@
 package com.moez.QKSMS.common.utils;
 
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.test.AndroidTestCase;
-import android.util.Log;
-
-import com.moez.QKSMS.ui.MainActivity;
 import com.moez.QKSMS.ui.settings.SettingsFragment;
 
 import java.text.SimpleDateFormat;
-import java.util.Locale;
 
 public class DateFormatterTest extends AndroidTestCase {
     protected void setUp() throws Exception {
@@ -16,7 +13,7 @@ public class DateFormatterTest extends AndroidTestCase {
     }
 
     public void test24Hours(){
-        SharedPreferences prefs = MainActivity.getPrefs(getContext());
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
         prefs.edit().putBoolean(SettingsFragment.TIMESTAMPS_24H, true).commit();
         assertTrue(DateFormatter.accountFor24HourTime(getContext(), new SimpleDateFormat("h:mm a")).equals(new SimpleDateFormat("H:mm")));
         prefs.edit().putBoolean(SettingsFragment.TIMESTAMPS_24H, false).commit();
