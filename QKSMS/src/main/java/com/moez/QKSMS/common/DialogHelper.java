e4a27ba7e9a1fe33967ca615273351f709ed10be
Moez Bhatti <moez.bhatti@gmail.com>
2015-10-31 11:57:24 -0400

    Improve messages for multi-delete

--- QKSMS/src/main/java/com/moez/QKSMS/common/DialogHelper.java
+++ QKSMS/src/main/java/com/moez/QKSMS/common/DialogHelper.java
@@ -22,21 +22,21 @@ public class DialogHelper {
     }
 
     public static void showDeleteConversationsDialog(final MainActivity context, final Set<Long> threadIds) {
-
         new DefaultSmsHelper(context, R.string.not_default_delete).showIfNotDefault(null);
 
+        Set<Long> threads = new HashSet<>(threadIds); // Make a copy so the list isn't reset when multi-select is disabled
         new QKDialog()
                 .setContext(context)
                 .setTitle(R.string.delete_conversation)
-                .setMessage(R.string.delete_confirmation)
+                .setMessage(context.getString(R.string.delete_confirmation, threads.size()))
                 .setPositiveButton(R.string.yes, v -> {
-                    Log.d(TAG, "Deleting threads: " + Arrays.toString(threadIds.toArray()));
+                    Log.d(TAG, "Deleting threads: " + Arrays.toString(threads.toArray()));
                     Conversation.ConversationQueryHandler handler = new Conversation.ConversationQueryHandler(context.getContentResolver());
-
-                    Conversation.startDelete(handler, 0, false, threadIds);
+                    Conversation.startDelete(handler, 0, false, threads);
                     Conversation.asyncDeleteObsoleteThreads(handler, 0);
                     context.showMenu();
-                }).setNegativeButton(R.string.cancel, null)
+                })
+                .setNegativeButton(R.string.cancel, null)
                 .show(context.getFragmentManager(), QKDialog.CONFIRMATION_TAG);
 
     }
@@ -44,17 +44,19 @@ public class DialogHelper {
     public static void showDeleteFailedMessagesDialog(final MainActivity context, final Set<Long> threadIds) {
         new DefaultSmsHelper(context, R.string.not_default_delete).showIfNotDefault(null);
 
+        Set<Long> threads = new HashSet<>(threadIds); // Make a copy so the list isn't reset when multi-select is disabled
         new QKDialog()
                 .setContext(context)
                 .setTitle(R.string.delete_all_failed)
-                .setMessage(R.string.delete_all_failed_confirmation)
+                .setMessage(context.getString(R.string.delete_all_failed_confirmation, threads.size()))
                 .setPositiveButton(R.string.yes, v -> {
                     new Thread(() -> {
-                        for (long threadId : threadIds) {
+                        for (long threadId : threads) {
                             SmsHelper.deleteFailedMessages(context, threadId);
                         }
                     }).start();
-                }).setNegativeButton(R.string.cancel, null)
+                })
+                .setNegativeButton(R.string.cancel, null)
                 .show(context.getFragmentManager(), QKDialog.CONFIRMATION_TAG);
     }
 
