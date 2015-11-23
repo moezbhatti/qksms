fa26442a7ac7762433539282633a86a5fa91c374
IhceneCh <ihcene.ch@gmail.com>
2015-11-22 21:21:19 -0500

    created two new tests in dataMessageTest.java

--- QKSMS/src/androidTest/java/com/orange/message/test/dataMessageTest.java
+++ QKSMS/src/androidTest/java/com/orange/message/test/dataMessageTest.java
@@ -2,14 +2,18 @@ package com.orange.message.test;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.test.AndroidTestCase;
 import android.widget.TextView;
 
 import com.moez.QKSMS.data.ContactHelper;
 import com.moez.QKSMS.data.Message;
 
+
+
 /**
  * Created by Joseph on 2015-11-22.
+ * Tests for before refactoring methods
  */
 public class dataMessageTest extends AndroidTestCase {
 
@@ -32,10 +36,16 @@ public class dataMessageTest extends AndroidTestCase {
     }
 
     public void testGetContactId() throws Exception {
-
+        Context context = getContext();
+        Message message = new Message(context, 0);
+        long testId = ContactHelper.getId(context, message.getAddress());
+        assertEquals(0, testId);
     }
 
     public void testGetPhotoBitmap() throws Exception {
-
+        Context context = getContext();
+        Message message = new Message(context, 0);
+        Bitmap bitmap = ContactHelper.getBitmap(context, message.getContactId());
+        assertEquals(null, bitmap);
     }
 }
