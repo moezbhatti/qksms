6fc19f0f1436508f28e2cffc9b35e0850fce5b87
IhceneCh <ihcene.ch@gmail.com>
2015-11-22 22:16:50 -0500

    Moved getId(), getName(), getPhotoBitMap() methods from ContactHelper to Contact. + Unit Test pass

--- QKSMS/src/androidTest/java/com/orange/message/test/dataMessageTest.java
+++ QKSMS/src/androidTest/java/com/orange/message/test/dataMessageTest.java
@@ -1,12 +1,10 @@
 package com.orange.message.test;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.test.AndroidTestCase;
-import android.widget.TextView;
 
-import com.moez.QKSMS.data.ContactHelper;
+import com.moez.QKSMS.data.Contact;
 import com.moez.QKSMS.data.Message;
 
 
@@ -31,21 +29,21 @@ public class dataMessageTest extends AndroidTestCase {
         Context context = getContext();
         long id = 2;
         Message message = new Message(context, id);
-        String name = ContactHelper.getName(context, message.getAddress());
+        String name = Contact.getName(context, message.getAddress());
         assertEquals(message.getName(), name);
     }
 
     public void testGetContactId() throws Exception {
         Context context = getContext();
         Message message = new Message(context, 0);
-        long testId = ContactHelper.getId(context, message.getAddress());
+        long testId = Contact.getId(context, message.getAddress());
         assertEquals(0, testId);
     }
 
     public void testGetPhotoBitmap() throws Exception {
         Context context = getContext();
         Message message = new Message(context, 0);
-        Bitmap bitmap = ContactHelper.getBitmap(context, message.getContactId());
+        Bitmap bitmap = Contact.getBitmap(context, message.getContactId());
         assertEquals(null, bitmap);
     }
 }
