a6af78790166b297e2c3934d7b989c8ca400c172
Moez Bhatti <moez.bhatti@gmail.com>
2016-01-04 12:45:55 -0500

    Fix notification click crash

--- QKSMS/src/main/java/com/moez/QKSMS/ui/MainActivity.java
+++ QKSMS/src/main/java/com/moez/QKSMS/ui/MainActivity.java
@@ -591,7 +591,7 @@ public class MainActivity extends QKActivity implements SlidingMenu.SlidingMenuL
     public void onClosed() {
         // When the menu (i.e. the conversation list) has been closed, the content has been opened.
         // So notify the content fragment.
-        if (mContent != null) mContent.onContentOpened();
+        if (mContent != null && ((Fragment) mContent).isAdded()) mContent.onContentOpened();
     }
 
     @Override
