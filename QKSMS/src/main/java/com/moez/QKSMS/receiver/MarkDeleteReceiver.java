ba29e18a91baaa4d7d437afa65815bf6a2f9b9a5
vijaysy <vijayyalasangimath@gmail.com>
2016-05-30 13:13:45 +0530

    unread bug fixed

--- QKSMS/src/main/java/com/moez/QKSMS/receiver/MarkDeleteReceiver.java
+++ QKSMS/src/main/java/com/moez/QKSMS/receiver/MarkDeleteReceiver.java
@@ -1,12 +1,13 @@
 package com.moez.QKSMS.receiver;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
-import com.moez.QKSMS.data.ConversationLegacy;
 import com.moez.QKSMS.transaction.NotificationManager;
 
 /**
@@ -20,34 +21,27 @@ public class MarkDeleteReceiver extends BroadcastReceiver {
         Bundle extras = intent.getExtras();
         long threadId = extras.getLong("thread_id");
         Uri mMessageUri = extras.getParcelable("mMessageUri");
-        ConversationLegacy conversation = new ConversationLegacy(context, threadId);
-        conversation.markRead();
-        new Thread(() -> {
-            context.getContentResolver().delete(mMessageUri, null, null);
-            NotificationManager.dismiss(context, (int) threadId);
-        }).start();
 
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                ContentValues values = new ContentValues();
+                values.put("read", true);
+                context.getContentResolver().update(mMessageUri, values, null, null);
+            }
 
-//        new AsyncTask<Void, Void, Void>() {
-//            @Override
-//            protected void onPreExecute() {
-//                super.onPreExecute();
-//                ConversationLegacy conversation = new ConversationLegacy(context, threadId);
-//                conversation.markRead();
-//            }
-//
-//            protected Void doInBackground(Void... none) {
-//                context.getContentResolver().delete(mMessageUri, null, null);
-//                return null;
-//            }
-//
-//            @Override
-//            protected void onPostExecute(Void aVoid) {
-//                super.onPostExecute(aVoid);
-//                NotificationManager.dismiss(context, (int) threadId);
-//                QKReplyActivity.dismiss(threadId);
-//            }
-//        }.execute();
+            protected Void doInBackground(Void... none) {
+                context.getContentResolver().delete(mMessageUri, null, null);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                NotificationManager.dismiss(context, (int) threadId);
+            }
+        }.execute();
 
 
     }
