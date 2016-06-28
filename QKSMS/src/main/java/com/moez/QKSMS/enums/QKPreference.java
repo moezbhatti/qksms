a432c0e2c6285f8cbe9ae40325e1af6609e575ed
Moez Bhatti <moez.bhatti@gmail.com>
2016-06-28 03:01:00 -0400

    Default setting for auto delete

--- QKSMS/src/main/java/com/moez/QKSMS/enums/QKPreference.java
+++ QKSMS/src/main/java/com/moez/QKSMS/enums/QKPreference.java
@@ -42,7 +42,7 @@ public enum QKPreference {
     DELIVERY_TOAST("pref_key_delivery_toast", true),
     DELIVERY_VIBRATE("pref_key_delivery_vibrate", true),
 
-    AUTO_DELETE("pref_key_delete_old_messages", false),
+    AUTO_DELETE("pref_key_delete_old_messages", true),
     AUTO_DELETE_UNREAD("pref_key_delete_old_unread_messages", "7"), // This type of preference only accepts strings
     AUTO_DELETE_READ("pref_key_delete_old_read_messages", "7"),
 
