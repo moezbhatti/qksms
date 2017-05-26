3ca4b364dc79d9f44c21fed07a0598fbc9715536
Moez Bhatti <moez.bhatti@gmail.com>
2017-05-25 22:20:04 -0400

    Initial project setup

---
+++ qksms/src/main/java/com/moez/QKSMS/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.moez.QKSMS
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
