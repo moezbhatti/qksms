01996f0e911640724749b021fd7490380eb511cb
Moez Bhatti <moez.bhatti@gmail.com>
2017-05-27 18:48:41 -0400

    Ask for READ_SMS permission on launch

--- qksms/src/main/java/com/moez/QKSMS/MainActivity.kt
+++ qksms/src/main/java/com/moez/QKSMS/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.moez.QKSMS
 
+import android.Manifest
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.READ_SMS)
+                .withListener(object : PermissionListener {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse) {
+                        Log.d(TAG, "Permission granted")
+                    }
+
+                    override fun onPermissionDenied(response: PermissionDeniedResponse) {
+                        Log.d(TAG, "Permission denied")
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(permission: PermissionRequest, token: PermissionToken) {
+                    }
+                })
+                .check()
+
     }
 }
