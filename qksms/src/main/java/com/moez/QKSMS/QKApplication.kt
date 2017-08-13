6826e66287b4c0cb4d652f956333ac4f164b20af
Moez Bhatti <moez.bhatti@gmail.com>
2017-08-13 16:16:53 -0400

    add bugsnag support

--- qksms/src/main/java/com/moez/QKSMS/QKApplication.kt
+++ qksms/src/main/java/com/moez/QKSMS/QKApplication.kt
@@ -1,6 +1,7 @@
 package com.moez.QKSMS
 
 import android.app.Application
+import com.bugsnag.android.Bugsnag
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
@@ -8,6 +9,8 @@ class QKApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Bugsnag.init(this)
 
         Realm.init(this)
         Realm.setDefaultConfiguration(RealmConfiguration.Builder()
