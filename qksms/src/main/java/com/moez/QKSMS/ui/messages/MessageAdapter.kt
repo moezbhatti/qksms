8b408c88a13499f28ffc422a4baede5269d5c73c
Moez Bhatti <moez.bhatti@gmail.com>
2017-08-13 22:51:21 -0400

    separate layout file for each message viewtype

--- qksms/src/main/java/com/moez/QKSMS/ui/messages/MessageAdapter.kt
+++ qksms/src/main/java/com/moez/QKSMS/ui/messages/MessageAdapter.kt
@@ -2,7 +2,6 @@ package com.moez.QKSMS.ui.messages
 
 import android.content.Context
 import android.util.Log
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.moez.QKSMS.R
@@ -14,9 +13,13 @@ class MessageAdapter(context: Context, data: OrderedRealmCollection<Message>?) :
         RealmRecyclerViewAdapter<Message, MessageViewHolder>(context, data, true) {
     val TAG = "MessageAdapter"
 
+    val VIEWTYPE_IN = 0
+    val VIEWTYPE_OUT = 1
+
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MessageViewHolder {
         val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        return MessageViewHolder(layoutInflater.inflate(R.layout.message_list_item, parent, false))
+        val layoutRes = if (viewType == VIEWTYPE_IN) R.layout.message_list_item_in else R.layout.message_list_item_out
+        return MessageViewHolder(layoutInflater.inflate(layoutRes, parent, false))
     }
 
     override fun onBindViewHolder(viewHolder: MessageViewHolder, position: Int) {
@@ -27,6 +30,10 @@ class MessageAdapter(context: Context, data: OrderedRealmCollection<Message>?) :
         }
 
         viewHolder.body.text = message.body
-        viewHolder.body.gravity = if (message.isMe()) Gravity.RIGHT else Gravity.LEFT
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val message = getItem(position)!!
+        return if (message.isMe()) VIEWTYPE_OUT else VIEWTYPE_IN
     }
 }
