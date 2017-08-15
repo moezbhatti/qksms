133af1cfbab29cb2dc5a7b5194b94d607cdf58f3
Moez Bhatti <moez.bhatti@gmail.com>
2017-08-14 23:13:13 -0400

    add classes necessary to be default sms app

---
+++ qksms/src/main/java/com/moez/QKSMS/receiver/SmsReceiver.kt
@@ -0,0 +1,13 @@
+package com.moez.QKSMS.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SmsReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+    }
+
+}
---
+++ qksms/src/main/java/com/moez/QKSMS/service/HeadlessSmsSendService.kt
@@ -0,0 +1,11 @@
+package com.moez.QKSMS.service
+
+import android.app.IntentService
+import android.content.Intent
+
+class HeadlessSmsSendService : IntentService("HeadlessSmsSendService") {
+
+    override fun onHandleIntent(p0: Intent?) {
+    }
+
+}
