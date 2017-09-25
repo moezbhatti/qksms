2497528b11e94afe83782de3c249b648def6b628
Moez Bhatti <moez.bhatti@gmail.com>
2017-09-25 17:18:09 -0400

    Avatar background colour

--- qksms/src/main/java/com/moez/QKSMS/ui/view/AvatarView.kt
+++ qksms/src/main/java/com/moez/QKSMS/ui/view/AvatarView.kt
@@ -1,11 +1,13 @@
 package com.moez.QKSMS.ui.view
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
 import com.moez.QKSMS.R
 import com.moez.QKSMS.data.model.Contact
+import com.moez.QKSMS.util.extensions.getColorCompat
 import kotlinx.android.synthetic.main.avatar_view.view.*
 
 class AvatarView(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : FrameLayout(context, attrs, defStyleAttr) {
@@ -25,7 +27,8 @@ class AvatarView(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : Fr
 
     override fun onFinishInflate() {
         super.onFinishInflate()
-        setBackgroundResource(R.drawable.message_only)
+        setBackgroundResource(R.drawable.circle)
+        backgroundTintList = ColorStateList.valueOf(context.getColorCompat(R.color.bubbleLight))
 
         if (!isInEditMode) {
             updateView()
