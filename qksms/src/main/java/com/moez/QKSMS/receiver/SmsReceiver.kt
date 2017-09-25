36389c9864d0ec6cb487fb9ca90ea9a3d458cc2e
Moez Bhatti <moez.bhatti@gmail.com>
2017-09-25 16:35:48 -0400

    Simplify logic for obtaining SmsMessages from Intent

--- qksms/src/main/java/com/moez/QKSMS/receiver/SmsReceiver.kt
+++ qksms/src/main/java/com/moez/QKSMS/receiver/SmsReceiver.kt
@@ -3,8 +3,7 @@ package com.moez.QKSMS.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
-import android.telephony.SmsMessage
+import android.provider.Telephony.Sms
 import com.moez.QKSMS.dagger.AppComponentManager
 import com.moez.QKSMS.data.repository.MessageRepository
 import timber.log.Timber
@@ -18,26 +17,15 @@ class SmsReceiver : BroadcastReceiver() {
         Timber.v("Received SMS: $intent")
         AppComponentManager.appComponent.inject(this)
 
-        intent.extras?.let { extras ->
-            val pdus = extras.get("pdus") as Array<Any>
-            val format = extras.getString("format")
-            val messages = pdus.map { pdu ->
-                if (Build.VERSION.SDK_INT > 23) {
-                    SmsMessage.createFromPdu(pdu as ByteArray, format)
-                } else {
-                    SmsMessage.createFromPdu(pdu as ByteArray)
-                }
-            }
+        val messages = Sms.Intents.getMessagesFromIntent(intent)
+        messages?.takeIf { it.isNotEmpty() }?.let { messages ->
+            val address = messages[0].displayOriginatingAddress
+            val time = messages[0].timestampMillis
+            val body: String = messages
+                    .map { message -> message.displayMessageBody }
+                    .reduce { body, new -> body + new }
 
-            messages[0]?.let { sms ->
-                val address = sms.displayOriginatingAddress
-                val time = sms.timestampMillis
-                val body: String = messages
-                        .map { message -> message.displayMessageBody }
-                        .reduce { body, new -> body + new }
-
-                messageRepo.insertMessage(address, body, time)
-            }
+            messageRepo.insertMessage(address, body, time)
         }
     }
 
