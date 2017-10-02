41aea8c618b20b34916f817884d60a7ab4a8b34f
Moez Bhatti <moez.bhatti@gmail.com>
2017-10-01 20:08:39 -0400

    Move message sending code into SendMessage.kt

--- qksms/src/main/java/com/moez/QKSMS/data/repository/MessageRepository.kt
+++ qksms/src/main/java/com/moez/QKSMS/data/repository/MessageRepository.kt
@@ -1,20 +1,14 @@
 package com.moez.QKSMS.data.repository
 
-import android.app.PendingIntent
 import android.content.ContentValues
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
-import android.provider.Telephony.Sms
-import android.telephony.SmsManager
 import com.moez.QKSMS.common.util.extensions.insertOrUpdate
 import com.moez.QKSMS.data.datasource.MessageTransaction
 import com.moez.QKSMS.data.model.Conversation
 import com.moez.QKSMS.data.model.Message
 import com.moez.QKSMS.data.sync.MessageColumns
 import com.moez.QKSMS.data.sync.SyncManager
-import com.moez.QKSMS.receiver.MessageDeliveredReceiver
-import com.moez.QKSMS.receiver.MessageSentReceiver
 import io.reactivex.Flowable
 import io.reactivex.schedulers.Schedulers
 import io.realm.Realm
@@ -79,33 +73,6 @@ class MessageRepository @Inject constructor(
     fun markRead(threadId: Long) {
         nativeMessageTransaction.markRead(threadId)
         realmMessageTransaction.markRead(threadId)
-    }
-
-    fun sendMessage(threadId: Long, address: String, body: String) {
-        val values = ContentValues()
-        values.put(Sms.ADDRESS, address)
-        values.put(Sms.BODY, body)
-        values.put(Sms.DATE, System.currentTimeMillis())
-        values.put(Sms.READ, true)
-        values.put(Sms.TYPE, Sms.MESSAGE_TYPE_OUTBOX)
-        values.put(Sms.THREAD_ID, threadId)
-
-        val contentResolver = context.contentResolver
-        Flowable.just(values)
-                .subscribeOn(Schedulers.io())
-                .map { contentResolver.insert(Sms.CONTENT_URI, values) }
-                .subscribe { uri ->
-                    addMessageFromUri(uri)
-
-                    val sentIntent = Intent(context, MessageSentReceiver::class.java).putExtra("uri", uri.toString())
-                    val sentPI = PendingIntent.getBroadcast(context, uri.lastPathSegment.toInt(), sentIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-                    val deliveredIntent = Intent(context, MessageDeliveredReceiver::class.java).putExtra("uri", uri.toString())
-                    val deliveredPI = PendingIntent.getBroadcast(context, uri.lastPathSegment.toInt(), deliveredIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-                    val smsManager = SmsManager.getDefault()
-                    smsManager.sendTextMessage(address, null, body, sentPI, deliveredPI)
-                }
     }
 
     fun updateMessageFromUri(values: ContentValues, uri: Uri) {
--- qksms/src/main/java/com/moez/QKSMS/domain/interactor/SendMessage.kt
+++ qksms/src/main/java/com/moez/QKSMS/domain/interactor/SendMessage.kt
@@ -1,16 +1,50 @@
 package com.moez.QKSMS.domain.interactor
 
+import android.app.PendingIntent
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.provider.Telephony
+import android.telephony.SmsManager
 import com.moez.QKSMS.data.repository.MessageRepository
+import com.moez.QKSMS.receiver.MessageDeliveredReceiver
+import com.moez.QKSMS.receiver.MessageSentReceiver
 import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
-class SendMessage @Inject constructor(val messageRepo: MessageRepository) : Interactor<Unit, SendMessage.Params>() {
+class SendMessage @Inject constructor(
+        val context: Context,
+        val messageRepo: MessageRepository) : Interactor<Unit, SendMessage.Params>() {
 
     data class Params(val threadId: Long, val address: String, val body: String)
 
     override fun buildUseCaseObservable(params: Params): Flowable<Unit> {
-        return Flowable.just(Unit)
-                .doOnNext { messageRepo.sendMessage(params.threadId, params.address, params.body) }
+        val values = ContentValues()
+        values.put(Telephony.Sms.ADDRESS, params.address)
+        values.put(Telephony.Sms.BODY, params.body)
+        values.put(Telephony.Sms.DATE, System.currentTimeMillis())
+        values.put(Telephony.Sms.READ, true)
+        values.put(Telephony.Sms.TYPE, Telephony.Sms.MESSAGE_TYPE_OUTBOX)
+        values.put(Telephony.Sms.THREAD_ID, params.threadId)
+
+        val contentResolver = context.contentResolver
+        return Flowable.just(values)
+                .subscribeOn(Schedulers.io())
+                .map { contentResolver.insert(Telephony.Sms.CONTENT_URI, values) }
+                .doOnNext { uri ->
+                    messageRepo.addMessageFromUri(uri)
+
+                    val sentIntent = Intent(context, MessageSentReceiver::class.java).putExtra("uri", uri.toString())
+                    val sentPI = PendingIntent.getBroadcast(context, uri.lastPathSegment.toInt(), sentIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+                    val deliveredIntent = Intent(context, MessageDeliveredReceiver::class.java).putExtra("uri", uri.toString())
+                    val deliveredPI = PendingIntent.getBroadcast(context, uri.lastPathSegment.toInt(), deliveredIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+                    val smsManager = SmsManager.getDefault()
+                    smsManager.sendTextMessage(params.address, null, params.body, sentPI, deliveredPI)
+                }
+                .map { Unit }
     }
 
 }
