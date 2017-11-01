8a313f026993f89a4bbd35c0bb8e74ce1585bda7
Moez Bhatti <moez.bhatti@gmail.com>
2017-11-01 15:11:00 -0700

    Add ability to change default SMS app in settings

--- qksms/src/main/java/com/moez/QKSMS/presentation/settings/SettingsViewModel.kt
+++ qksms/src/main/java/com/moez/QKSMS/presentation/settings/SettingsViewModel.kt
@@ -1,5 +1,8 @@
 package com.moez.QKSMS.presentation.settings
 
+import android.content.Context
+import android.content.Intent
+import android.provider.Telephony
 import com.moez.QKSMS.common.di.AppComponentManager
 import com.moez.QKSMS.domain.interactor.SyncConversations
 import com.moez.QKSMS.presentation.base.QkViewModel
@@ -8,6 +11,7 @@ import javax.inject.Inject
 
 class SettingsViewModel : QkViewModel<SettingsView, SettingsState>(SettingsState()) {
 
+    @Inject lateinit var context: Context
     @Inject lateinit var syncConversations: SyncConversations
 
     init {
@@ -21,6 +25,14 @@ class SettingsViewModel : QkViewModel<SettingsView, SettingsState>(SettingsState
             Timber.v("Preference click: ${it.key}")
 
             when (it.key) {
+                "defaultSms" -> {
+                    val intent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
+                    if (Telephony.Sms.getDefaultSmsPackage(context) != context.packageName) {
+                        intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, context.packageName)
+                    }
+                    context.startActivity(intent)
+                }
+
                 "sync" -> {
                     newState { it.copy(syncing = true) }
                     syncConversations.execute(Unit, {
