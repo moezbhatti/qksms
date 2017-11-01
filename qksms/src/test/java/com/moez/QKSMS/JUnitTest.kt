cc90d721464f77d6deee30f99cad8f1127fa5df0
Moez Bhatti <moez.bhatti@gmail.com>
2017-11-01 16:34:04 -0700

    Set up JUnit

---
+++ qksms/src/test/java/com/moez/QKSMS/JUnitTest.kt
@@ -0,0 +1,15 @@
+package com.moez.QKSMS
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class JUnitTest {
+
+    @Test
+    fun twoPlusTwoEqualsFour() {
+        assert(2 + 2 == 4)
+    }
+
+}
