21ac52263f30f7bdf41cb35bb7eb90042d4fbb1c
Moez Bhatti <moez.bhatti@gmail.com>
2017-11-23 14:23:47 -0500

    Install MixpanelAPI

--- qksms/src/main/java/com/moez/QKSMS/common/QKApplication.kt
+++ qksms/src/main/java/com/moez/QKSMS/common/QKApplication.kt
@@ -2,6 +2,7 @@ package com.moez.QKSMS.common
 
 import android.app.Application
 import com.bugsnag.android.Bugsnag
+import com.mixpanel.android.mpmetrics.MixpanelAPI
 import com.moez.QKSMS.common.di.AppComponentManager
 import io.realm.Realm
 import io.realm.RealmConfiguration
@@ -14,6 +15,8 @@ class QKApplication : Application() {
         super.onCreate()
 
         Bugsnag.init(this)
+
+        MixpanelAPI.getInstance(this, "")
 
         Realm.init(this)
         Realm.setDefaultConfiguration(RealmConfiguration.Builder()
