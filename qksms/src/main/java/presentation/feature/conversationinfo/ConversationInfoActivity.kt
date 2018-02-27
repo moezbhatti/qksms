93f7c06acbb224f6e68c58bd7963ca9bddec7102
Moez Bhatti <moez.bhatti@gmail.com>
2018-02-26 23:46:49 -0500

    Only show notification channels for conversation on Android O

--- qksms/src/main/java/presentation/feature/conversationinfo/ConversationInfoActivity.kt
+++ qksms/src/main/java/presentation/feature/conversationinfo/ConversationInfoActivity.kt
@@ -18,6 +18,7 @@
  */
 package presentation.feature.conversationinfo
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import com.jakewharton.rxbinding2.view.clicks
@@ -25,6 +26,7 @@ import com.moez.QKSMS.R
 import com.uber.autodispose.android.lifecycle.scope
 import com.uber.autodispose.kotlin.autoDisposable
 import common.di.appComponent
+import common.util.extensions.setVisible
 import kotlinx.android.synthetic.main.conversation_info_activity.*
 import presentation.common.base.QkThemedActivity
 import javax.inject.Inject
@@ -52,6 +54,9 @@ class ConversationInfoActivity : QkThemedActivity<ConversationInfoViewModel>(), 
         colors.background
                 .autoDisposable(scope())
                 .subscribe { color -> window.decorView.setBackgroundColor(color) }
+
+        val supportsNotificationChannels = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+        notifications.setVisible(supportsNotificationChannels)
 
         recipients.layoutManager = LinearLayoutManager(this)
         recipients.adapter = recipientAdapter
