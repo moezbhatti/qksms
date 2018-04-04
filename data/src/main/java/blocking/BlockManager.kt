f21d7ed54a12b1be60254957fe989b3c4dda0cc1
Moez Bhatti <moez.bhatti@gmail.com>
2018-04-04 16:13:46 -0400

    Flatten logic in isBlocked

--- data/src/main/java/blocking/BlockManager.kt
+++ data/src/main/java/blocking/BlockManager.kt
@@ -8,38 +8,35 @@ import javax.inject.Inject
 class BlockManager @Inject constructor(private val context: Context) {
 
     fun isBlocked(address: String) {
-        val isShouldIAnswerInstalled = try {
+        try {
             context.packageManager.getApplicationInfo("org.mistergroup.shouldianswerpersonal", 0).enabled
         } catch (e: PackageManager.NameNotFoundException) {
-            Timber.w(e)
-            false
+            return
         }
 
-        if (isShouldIAnswerInstalled) {
-            val shouldIAnswerBinder = ShouldIAnswerBinder()
-            shouldIAnswerBinder.callback = object : ShouldIAnswerBinder.Callback {
+        val shouldIAnswerBinder = ShouldIAnswerBinder()
+        shouldIAnswerBinder.callback = object : ShouldIAnswerBinder.Callback {
 
-                override fun onNumberRating(number: String?, rating: Int) {
-                    Timber.i("onNumberRating " + number + ": " + rating.toString())
-                    shouldIAnswerBinder.unbind(context)
+            override fun onNumberRating(number: String?, rating: Int) {
+                Timber.i("onNumberRating " + number + ": " + rating.toString())
+                shouldIAnswerBinder.unbind(context)
 
-                    Timber.v("Should block: ${rating == ShouldIAnswerBinder.RATING_NEGATIVE}")
-                }
-
-                override fun onServiceConnected() {
-                    try {
-                        shouldIAnswerBinder.getNumberRating(address)
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    }
-
-                }
-
-                override fun onServiceDisconnected() {}
+                Timber.v("Should block: ${rating == ShouldIAnswerBinder.RATING_NEGATIVE}")
             }
 
-            shouldIAnswerBinder.bind(context)
+            override fun onServiceConnected() {
+                try {
+                    shouldIAnswerBinder.getNumberRating(address)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+            }
+
+            override fun onServiceDisconnected() {}
         }
+
+        shouldIAnswerBinder.bind(context)
     }
 
 }
