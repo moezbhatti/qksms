321df66ac4e8107a5e33285caf732ac59373e8f9
Adam Niederer <adam.niederer@gmail.com>
2018-04-25 17:21:43 -0400

    Search conversation snippets as well as recipients

--- presentation/src/main/java/common/util/filter/ConversationFilter.kt
+++ presentation/src/main/java/common/util/filter/ConversationFilter.kt
@@ -24,7 +24,11 @@ import javax.inject.Inject
 class ConversationFilter @Inject constructor(private val recipientFilter: RecipientFilter) : Filter<Conversation>() {
 
     override fun filter(item: Conversation, query: CharSequence): Boolean {
-        return item.recipients.any { recipient -> recipientFilter.filter(recipient, query) }
+        return item.recipients.any { recipient -> recipientFilter.filter(recipient, query) } ||
+                if (query.matches(Regex("/.*/")))
+                    Regex(query.trim('/').toString()).containsMatchIn(item.snippet)
+                else
+                    item.snippet.contains(query)
     }
 
 }
