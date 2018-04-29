58b9f0553220f45c7d647313edabfa3ed37a2465
Moez Bhatti <moez.bhatti@gmail.com>
2018-04-29 16:48:48 -0400

    #1070 - Update conversation when message is sent from another app

--- domain/src/main/java/interactor/SyncMessage.kt
+++ domain/src/main/java/interactor/SyncMessage.kt
@@ -21,15 +21,20 @@ package interactor
 import android.net.Uri
 import io.reactivex.Flowable
 import model.Message
+import repository.MessageRepository
 import repository.SyncRepository
 import util.extensions.mapNotNull
 import javax.inject.Inject
 
-class SyncMessage @Inject constructor(private val syncManager: SyncRepository) : Interactor<Uri>() {
+class SyncMessage @Inject constructor(
+        private val messageRepo: MessageRepository,
+        private val syncManager: SyncRepository
+) : Interactor<Uri>() {
 
     override fun buildObservable(params: Uri): Flowable<Message> {
         return Flowable.just(params)
                 .mapNotNull { uri -> syncManager.syncMessage(uri) }
+                .doOnNext { message -> messageRepo.updateConversation(message.threadId) }
     }
 
 }
