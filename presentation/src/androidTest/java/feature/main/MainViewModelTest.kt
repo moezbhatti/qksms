697f665674ebf0c7bc1f615aca0fc7efb7054389
Moez Bhatti <moez.bhatti@gmail.com>
2018-05-10 14:37:25 -0400

    Update testing dependencies

---
+++ presentation/src/androidTest/java/feature/main/MainViewModelTest.kt
@@ -0,0 +1,15 @@
+package feature.main
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainViewModelTest {
+
+    @Test
+    fun insertedSentMessageEqualsSyncResult() {
+        assert(1 == 1)
+    }
+
+}
