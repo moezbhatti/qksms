b2f5ccedf93ca10d5b2931a72709fa06a186a7f0
Moez Bhatti <moez.bhatti@gmail.com>
2018-07-24 15:07:27 -0400

    #1233 - Dismiss notification when deleting conversation

--- domain/src/main/java/com/moez/QKSMS/interactor/DeleteConversations.kt
+++ domain/src/main/java/com/moez/QKSMS/interactor/DeleteConversations.kt
@@ -18,18 +18,21 @@
  */
 package com.moez.QKSMS.interactor
 
+import com.moez.QKSMS.manager.NotificationManager
 import com.moez.QKSMS.repository.ConversationRepository
 import io.reactivex.Flowable
 import javax.inject.Inject
 
 class DeleteConversations @Inject constructor(
         private val conversationRepo: ConversationRepository,
+        private val notificationManager: NotificationManager,
         private val updateBadge: UpdateBadge
 ) : Interactor<List<Long>>() {
 
     override fun buildObservable(params: List<Long>): Flowable<*> {
         return Flowable.just(params.toLongArray())
                 .doOnNext { threadIds -> conversationRepo.deleteConversations(*threadIds) }
+                .doOnNext { threadIds -> threadIds.forEach(notificationManager::update) }
                 .flatMap { updateBadge.buildObservable(Unit) } // Update the badge
     }
 
