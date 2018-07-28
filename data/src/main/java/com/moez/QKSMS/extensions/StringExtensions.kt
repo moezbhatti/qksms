355fa5e6c6bbcffae4fd1ea049dc6a567952c720
KruziikG <licharthas@foxmail.com>
2018-07-28 12:22:07 +0800

    #1156 - Fix search for non-ascii characters

--- data/src/main/java/com/moez/QKSMS/extensions/StringExtensions.kt
+++ data/src/main/java/com/moez/QKSMS/extensions/StringExtensions.kt
@@ -24,4 +24,3 @@ import java.text.Normalizer
  * Strip the accents from a string
  */
 fun CharSequence.removeAccents() = Normalizer.normalize(this, Normalizer.Form.NFD)
-        .replace(Regex("[^\\p{ASCII}]"), "")
