9b26e7306ef6d8400bc8fdab6a7647e44709f372
Moez Bhatti <moez.bhatti@gmail.com>
2018-09-04 21:36:04 -0400

    Fix crash when trying to update contact that exists

--- data/src/main/java/com/moez/QKSMS/repository/SyncRepositoryImpl.kt
+++ data/src/main/java/com/moez/QKSMS/repository/SyncRepositoryImpl.kt
@@ -251,7 +251,7 @@ class SyncRepositoryImpl @Inject constructor(
             val recipients = realm.where(Recipient::class.java).findAll()
 
             realm.executeTransaction {
-                contact = realm.copyToRealm(contact)
+                contact = realm.copyToRealmOrUpdate(contact)
 
                 // Update all the matching recipients with the new contact
                 val updatedRecipients = recipients
