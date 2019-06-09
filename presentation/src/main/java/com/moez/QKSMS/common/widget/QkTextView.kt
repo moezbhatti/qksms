97f447dfd3d0f06943a23c5f40cf40abfb9814f0
moezbhatti <moez.bhatti@gmail.com>
2019-06-09 01:44:14 -0400

    #1393 - Improve how we display long conversation names or group members

--- presentation/src/main/java/com/moez/QKSMS/common/widget/QkTextView.kt
+++ presentation/src/main/java/com/moez/QKSMS/common/widget/QkTextView.kt
@@ -30,12 +30,42 @@ open class QkTextView @JvmOverloads constructor(context: Context, attrs: Attribu
 
     @Inject lateinit var textViewStyler: TextViewStyler
 
+    /**
+     * Collapse a multiline list of strings into a single line
+     *
+     * Ex.
+     *
+     * Toronto, New York, Los Angeles,
+     * Seattle, Portland
+     *
+     * Will be converted to
+     *
+     * Toronto, New York, Los Angeles, +2
+     */
+    var collapseEnabled: Boolean = false
+
     init {
         if (!isInEditMode) {
             appComponent.inject(this)
             textViewStyler.applyAttributes(this, attrs)
         } else {
             TextViewStyler.applyEditModeAttributes(this, attrs)
+        }
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+
+        if (collapseEnabled) {
+            layout
+                    ?.takeIf { layout -> layout.lineCount > 0 }
+                    ?.let { layout -> layout.getEllipsisCount(layout.lineCount - 1) }
+                    ?.takeIf { ellipsisCount -> ellipsisCount > 0 }
+                    ?.let { ellipsisCount ->
+                        val lastComma = text.dropLast(ellipsisCount).lastIndexOf(',')
+                        val remainingNames = text.drop(lastComma).count { c -> c == ',' }
+                        text = "${text.take(lastComma)}, +$remainingNames"
+                    }
         }
     }
 
