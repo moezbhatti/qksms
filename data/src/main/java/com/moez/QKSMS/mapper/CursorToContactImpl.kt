f64d1befb1f7cbf481e3ad395b2a7571304e2ed2
moezbhatti <moez.bhatti@gmail.com>
2019-09-17 23:58:34 -0400

    #1328 - Show label for custom number

--- data/src/main/java/com/moez/QKSMS/mapper/CursorToContactImpl.kt
+++ data/src/main/java/com/moez/QKSMS/mapper/CursorToContactImpl.kt
@@ -20,7 +20,7 @@ package com.moez.QKSMS.mapper
 
 import android.content.Context
 import android.database.Cursor
-import android.provider.ContactsContract.CommonDataKinds.Phone.*
+import android.provider.ContactsContract.CommonDataKinds.Phone
 import com.moez.QKSMS.manager.PermissionManager
 import com.moez.QKSMS.model.Contact
 import com.moez.QKSMS.model.PhoneNumber
@@ -32,23 +32,32 @@ class CursorToContactImpl @Inject constructor(
 ) : CursorToContact {
 
     companion object {
-        val URI = CONTENT_URI
-        val PROJECTION = arrayOf(LOOKUP_KEY, NUMBER, TYPE, DISPLAY_NAME, CONTACT_LAST_UPDATED_TIMESTAMP)
+        val URI = Phone.CONTENT_URI
+        val PROJECTION = arrayOf(
+                Phone.LOOKUP_KEY,
+                Phone.NUMBER,
+                Phone.TYPE,
+                Phone.LABEL,
+                Phone.DISPLAY_NAME,
+                Phone.CONTACT_LAST_UPDATED_TIMESTAMP
+        )
 
         const val COLUMN_LOOKUP_KEY = 0
         const val COLUMN_NUMBER = 1
         const val COLUMN_TYPE = 2
-        const val COLUMN_DISPLAY_NAME = 3
-        const val CONTACT_LAST_UPDATED = 4
+        const val COLUMN_LABEL = 3
+        const val COLUMN_DISPLAY_NAME = 4
+        const val CONTACT_LAST_UPDATED = 5
     }
 
     override fun map(from: Cursor) = Contact().apply {
         lookupKey = from.getString(COLUMN_LOOKUP_KEY)
         name = from.getString(COLUMN_DISPLAY_NAME) ?: ""
-        numbers.add(PhoneNumber().apply {
-            address = from.getString(COLUMN_NUMBER) ?: ""
-            type = context.getString(getTypeLabelResource(from.getInt(COLUMN_TYPE)))
-        })
+        numbers.add(PhoneNumber(
+                address = from.getString(COLUMN_NUMBER) ?: "",
+                type = Phone.getTypeLabel(context.resources, from.getInt(COLUMN_TYPE),
+                        from.getString(COLUMN_LABEL)).toString()
+        ))
         lastUpdate = from.getLong(CONTACT_LAST_UPDATED)
     }
 
