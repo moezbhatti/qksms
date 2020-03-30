b56111ab0843d542eb10e7d9fd07df0640e40edd
Moez Bhatti <moez.bhatti@gmail.com>
2020-03-29 21:45:59 -0400

    Allow sharing vCard from other apps

--- presentation/src/main/java/com/moez/QKSMS/feature/compose/ComposeActivityModule.kt
+++ presentation/src/main/java/com/moez/QKSMS/feature/compose/ComposeActivityModule.kt
@@ -20,7 +20,9 @@ package com.moez.QKSMS.feature.compose
 
 import android.content.Intent
 import android.net.Uri
+import androidx.core.net.toFile
 import androidx.lifecycle.ViewModel
+import com.google.android.mms.ContentType
 import com.moez.QKSMS.injection.ViewModelKey
 import com.moez.QKSMS.model.Attachment
 import com.moez.QKSMS.model.Attachments
@@ -28,6 +30,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
 import java.net.URLDecoder
+import java.nio.charset.Charset
 import javax.inject.Named
 
 @Module
@@ -73,10 +76,25 @@ class ComposeActivityModule {
     @Provides
     @Named("attachments")
     fun provideSharedAttachments(activity: ComposeActivity): Attachments {
-        val sharedImages = mutableListOf<Uri>()
-        activity.intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)?.run(sharedImages::add)
-        activity.intent.getParcelableArrayListExtra<Uri>(Intent.EXTRA_STREAM)?.run(sharedImages::addAll)
-        return Attachments(sharedImages.map { Attachment.Image(it) })
+        val uris = mutableListOf<Uri>()
+        activity.intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)?.run(uris::add)
+        activity.intent.getParcelableArrayListExtra<Uri>(Intent.EXTRA_STREAM)?.run(uris::addAll)
+        return Attachments(uris.mapNotNull { uri ->
+            val mimeType = activity.contentResolver.getType(uri)
+            when {
+                ContentType.isImageType(mimeType) -> {
+                    Attachment.Image(uri)
+                }
+
+                ContentType.TEXT_VCARD.equals(mimeType, true) -> {
+                    val inputStream = activity.contentResolver.openInputStream(uri)
+                    val text = inputStream?.reader(Charset.forName("utf-8"))?.readText()
+                    text?.let(Attachment::Contact)
+                }
+
+                else -> null
+            }
+        })
     }
 
     @Provides
@@ -88,7 +106,7 @@ class ComposeActivityModule {
     private fun Intent.decodedDataString(): String? {
         val data = data?.toString()
         if (data?.contains('%') == true) {
-           return URLDecoder.decode(data, "UTF-8")
+            return URLDecoder.decode(data, "UTF-8")
         }
         return data
     }
